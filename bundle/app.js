7f9b1e2ce51e760e088bce542760983dba6092fe
Rafał Mikołajun <rafal@mikoweb.pl>
2017-09-26 00:57:03 +0200

    bundle build

--- bundle/app.js
+++ bundle/app.js
@@ -1,8 +1,9 @@
-(function (Mn,_) {
+(function (Mn,_,$) {
 'use strict';
 
 Mn = Mn && Mn.hasOwnProperty('default') ? Mn['default'] : Mn;
 _ = _ && _.hasOwnProperty('default') ? _['default'] : _;
+$ = $ && $.hasOwnProperty('default') ? $['default'] : $;
 
 /**
  * @param {Object} object
@@ -51,7 +52,7 @@ var DrawerToggle = function (_Mn$View) {
     }, {
         key: '_onClick',
         value: function _onClick(e) {
-            var id = this.$el.attr('data-drawer-id');
+            var id = e.target.getAttribute('data-drawer-id');
 
             if (_.isString(id) && id.length > 0) {
                 var drawer = document.getElementById(id);
@@ -63,6 +64,81 @@ var DrawerToggle = function (_Mn$View) {
         }
     }]);
     return DrawerToggle;
+}(Mn.View);
+
+var PageProgress = function (_Mn$View) {
+    babelHelpers.inherits(PageProgress, _Mn$View);
+
+    function PageProgress() {
+        babelHelpers.classCallCheck(this, PageProgress);
+        return babelHelpers.possibleConstructorReturn(this, (PageProgress.__proto__ || Object.getPrototypeOf(PageProgress)).apply(this, arguments));
+    }
+
+    babelHelpers.createClass(PageProgress, [{
+        key: 'initialize',
+        value: function initialize() {
+            var _this2 = this;
+
+            this._reset();
+
+            $(document).ready(function () {
+                _this2._docReady = true;
+                _this2._loadUpdate();
+            });
+
+            WCReady(function () {
+                _this2._wcReady = true;
+                _this2._loadUpdate();
+            });
+
+            window.addEventListener('beforeunload', function () {
+                _this2._reset();
+                _this2.$el.addClass('active');
+                _this2.$el.attr('value', '5');
+            });
+        }
+    }, {
+        key: 'template',
+        value: function template() {
+            return _.noop;
+        }
+    }, {
+        key: 'el',
+        value: function el() {
+            return '#page-progress';
+        }
+    }, {
+        key: 'onRender',
+        value: function onRender() {
+            this.$el.addClass('active');
+        }
+    }, {
+        key: '_reset',
+        value: function _reset() {
+            this._loaded = false;
+            this._docReady = false;
+            this._wcReady = false;
+            this.$el.attr('value', '0');
+        }
+    }, {
+        key: '_loadUpdate',
+        value: function _loadUpdate() {
+            var _this3 = this;
+
+            if (this._docReady && this._wcReady) {
+                this.$el.attr('value', '100');
+                setTimeout(function () {
+                    _this3.$el.removeClass('active');
+                }, 500);
+                this._loaded = true;
+            } else if (this._docReady || this._wcReady) {
+                this.$el.attr('value', '40');
+            } else {
+                this.$el.attr('value', '0');
+            }
+        }
+    }]);
+    return PageProgress;
 }(Mn.View);
 
 var Html = function (_Mn$View) {
@@ -89,6 +165,7 @@ var Html = function (_Mn$View) {
             WCReady(function () {
                 $('[wc-hidden], [wc-lazy]').addClass('ready');
                 new DrawerToggle().render();
+                new PageProgress().render();
             });
         }
     }]);
@@ -100,5 +177,5 @@ name(window, 'App', {});
 
 new Html().render();
 
-}(Mn,_));
+}(Mn,_,$));
 //# sourceMappingURL=app.js.map
