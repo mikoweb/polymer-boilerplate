9fcd87fc8e0723e16c0af82a2a8c5aa5c6d44d93
Rafał Mikołajun <root@rmweb.pl>
2017-12-17 23:35:55 +0100

    add Redux

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ gulp.task('bundle-core', () => {
         Path.nodeModules('/axios/dist/axios.js'),
         Path.nodeModules('/objectmodel/dist/object-model.umd.js'),
         Path.nodeModules('/model-persistence/bundle/model-persistence.js'),
+        Path.nodeModules('/redux/dist/redux.js'),
         Path.bowerComponents('/wc-ready/index.js'),
     ], 'core.js');
 });
