30fe039dd248131ce685e226b7532575dc0d0e42
Rafał Mikołajun <root@rmweb.pl>
2019-01-20 17:01:35 +0100

    replace gulp-style-modules with gulp-polymer-styles

--- src/elements/hello-element/style.js
+++ src/elements/hello-element/style.js
@@ -1,8 +1,5 @@
 import { html } from '@polymer/polymer/polymer-element.js';
-
 export default html`<style>
-.hello-element {
-  background: var(--paper-indigo-100);
-  padding: 20px;
-}
+.hello-element{background:var(--paper-indigo-100);padding:20px}
+
 </style>`;
