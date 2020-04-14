623c3f7152fc829400c8102213c96f796a3e35b4
Rafał Mikołajun <root@rmweb.pl>
2020-04-14 17:12:51 +0200

    add class AbstractAppState

---
+++ src/elements/AbstractAppState.js
@@ -0,0 +1,28 @@
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+class AbstractAppState extends PolymerElement {
+    ready() {
+        super.ready();
+        this.style.display = 'none';
+    }
+
+    /**
+     * @param {string} property
+     * @param {function} listener
+     */
+    observeState(property, listener) {
+        this._createPropertyObserver(property, listener);
+    }
+
+    /**
+     * @param {string} property
+     * @param {function} listener
+     */
+    whenPropertyInit(property, listener) {
+        if (typeof this[property] !== 'undefined') {
+            listener(this[property]);
+        }
+
+        this.observeState(property, listener);
+    }
+}
