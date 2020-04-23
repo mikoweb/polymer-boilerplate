90bb44fe800214eeaf1dc277cdadb434a09b96a3
Rafał Mikołajun <root@rmweb.pl>
2020-04-23 09:20:34 +0200

    add class AbstractAppState

--- src/elements/AbstractAppState.js
+++ src/elements/AbstractAppState.js
@@ -1,6 +1,11 @@
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
 export default class AbstractAppState extends PolymerElement {
+    constructor() {
+        super();
+        this._observeOnceListenersCallsMap = new WeakMap();
+    }
+
     ready() {
         super.ready();
         this.style.display = 'none';
@@ -9,20 +14,39 @@ export default class AbstractAppState extends PolymerElement {
     /**
      * @param {string} property
      * @param {function} listener
+     * @param {boolean} once
      */
-    observeState(property, listener) {
-        this._createPropertyObserver(property, listener);
+    observeState(property, listener, once = false) {
+        if (once) {
+            const onceListener = ((state, registeredListener) => {
+                return (value) => {
+                    if (!state._observeOnceListenersCallsMap.has(registeredListener)) {
+                        state._observeOnceListenersCallsMap.set(registeredListener, true);
+                        registeredListener(value);
+                    }
+                };
+            })(this, listener);
+
+            this._createPropertyObserver(property, onceListener);
+        } else {
+            this._createPropertyObserver(property, listener);
+        }
     }
 
     /**
      * @param {string} property
      * @param {function} listener
+     * @param {boolean} once
      */
-    whenPropertyInit(property, listener) {
+    whenPropertyChange(property, listener, once = false) {
         if (typeof this[property] !== 'undefined') {
             listener(this[property]);
-        }
 
-        this.observeState(property, listener);
+            if (!once) {
+                this.observeState(property, listener, once);
+            }
+        } else {
+            this.observeState(property, listener, once);
+        }
     }
 }
